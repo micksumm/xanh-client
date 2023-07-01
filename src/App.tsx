f888f472558e097cb3899169ae71a75a5b63a32e
Summer <summmick@gmail.com>
2023-07-01 15:18:32 -0700

    Add ability to update a plant

--- src/App.tsx
+++ src/App.tsx
@@ -2,6 +2,7 @@ import React, { useState, useEffect} from 'react';
 import './App.css';
 import axios from 'axios';
 import CreatePlantComponent from './CreatePlantComponent';
+import EditPlantComponent from './EditPlantComponent';
 
 interface Plant {
   name: string;
@@ -24,6 +25,7 @@ function App() {
   return (
     <div>
       <CreatePlantComponent/>
+      <EditPlantComponent/>
       <div className="App">
         <div className="card-container">
           {plants.map((plant) => (
